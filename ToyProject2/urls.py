dbd05a92f3dceb20bb865216bb28cbf711e2c66b
an dong woo <akskwha@naver.com>
2023-04-04 22:31:21 +0900

    홈화면(로그인)

--- ToyProject2/urls.py
+++ ToyProject2/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from stopwatch import views
 urlpatterns = [
+    path("", views.home, name="home"),
     path('admin/', admin.site.urls),
     path('stopwatch/',include('stopwatch.urls')),
 ]
--- stopwatch/views.py
+++ stopwatch/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def home(request):#로그인 화면
+    return render(request, "stopwatch/home.html")
 def index(request):
     return render(request, "stopwatch/index.html")
