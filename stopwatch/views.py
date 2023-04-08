88812f04a7de42cb67d768fe8c1e686b0bee04bc
an dong woo <akskwha@naver.com>
2023-04-08 10:07:30 +0900

    스톱워치(미완성 유저 필요)

--- stopwatch/views.py
+++ stopwatch/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+import time
 # Create your views here.
 
 
 def home(request):#로그인 화면
     return render(request, "stopwatch/home.html")
+
 def index(request):#시작
-    return render(request, "stopwatch/index.html")
-def stop(request):#정지
-    return render(request, "stopwatch/stop.html")
+    if not is_running:
+        is_running=True#DB추가 기본 False
+        start_time =time.time()
+        print(start_time)
+    return redirect("stopwatch:stop")
+def stop(request):
+    is_running = False#DB추가 
+    return redirect("stopwatch:index")
