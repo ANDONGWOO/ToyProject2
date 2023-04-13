351285ab7f0703c12cf73d5b9fcb3bc67cef9188
an dong woo <akskwha@naver.com>
2023-04-14 06:04:27 +0900

    redirect 수정

--- stopwatch/views.py
+++ stopwatch/views.py
@@ -7,11 +7,11 @@ def home(request):#로그인 화면
     return render(request, "stopwatch/home.html")
 
 def index(request):#시작
-    if not is_running:
-        is_running=True#DB추가 기본 False
-        start_time =time.time()
-        print(start_time)
-    return redirect("stopwatch:stop")
+    # if not is_running:
+    #     is_running=True#DB추가 기본 False
+    #     start_time =time.time()
+    #     print(start_time)
+    return render(request, "stopwatch/stop.html")
 def stop(request):
-    is_running = False#DB추가 
-    return redirect("stopwatch:index")
+    # is_running = False#DB추가 
+    return render(request, "stopwatch/index.html")
