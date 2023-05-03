063399e0160394946622060114afd025fe774912
an dong woo <akskwha@naver.com>
2023-05-03 23:37:45 +0900

    test3(미구현)

--- static/js/stopwatch.js
+++ static/js/stopwatch.js
@@ -1,13 +1,19 @@
 var start;
 var end;
 var end_time;
+var start_now;
+
 function test1() {
-    start = new Date().getTime();
+    const start = new Date();
+    // start_now=new Date();
+    //start-start_now 화면에서 보여주기
+}
+function test2() {
+    const end = new Date();
+    end_time = new Date(end-start);
+    console.log(end_time)//DB
 }
 
-function test2() {
-    end = new Date().getTime();
-    end_time = new Date(end-start);
-    console.log(end_time.getMinutes())
-}
+
+
 
