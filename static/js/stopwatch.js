9d96974d04c683118b18d5b22529960702900965
an dong woo <akskwha@naver.com>
2023-05-04 19:15:33 +0900

    중단 메소드/일정시간후 실행 메소드 추가/스톱워치 화면보여주기x

--- static/js/stopwatch.js
+++ static/js/stopwatch.js
@@ -4,16 +4,33 @@ var end_time;
 var start_now;
 
 function test1() {
-    const start = new Date();
+    start = new Date().getTime();
     // start_now=new Date();
     //start-start_now 화면에서 보여주기
 }
 function test2() {
-    const end = new Date();
+    end = new Date();
     end_time = new Date(end-start);
-    console.log(end_time)//DB
+    min = end_time.getMinutes()
+    sec = end_time.getSeconds()
+    milisec = end_time.getMilliseconds()
+
+    document.getElementById("testM").innerText =min
+    document.getElementById("tests").innerText =sec
+    document.getElementById("testm").innerText =milisec
 }
 
+// function test3(){
+//     let count = 0;
+//     setInterval(() => console.log(++count), 1000);
+// }
 
+// function test4(){
+//     let count = 0;
+//     setInterval(() => console.log(++count), 1000);
+// }
+// test3()
+//clearInterval(변수) 중단
+//일정시간 후 실행 setInterval(() => console.log(++count), 1000);
 
 
